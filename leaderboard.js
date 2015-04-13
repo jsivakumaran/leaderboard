4ada9b69f4f28d3be85e3de4c0bd204a28facf12
jsivakumaran <jsivakumaran@outlook.com>
2015-04-12 22:50:24 -0700

    add highlighting of clicked player in leaderboard

--- leaderboard.js
+++ leaderboard.js
@@ -2,16 +2,21 @@ PlayersList = new Mongo.Collection('players');
 
 if (Meteor.isClient){
 	Template.leaderboard.events({
-		'click': function(){
-		  console.log('You clicked something');
+		'click .player': function(){
+		  var playerId = this._id;
+		  Session.set('selectedPlayer', playerId);
 		}
 	});
 	Template.leaderboard.helpers({
 		'player': function(){
 			return PlayersList.find()
 		},
-		'otherHelperFunction': function(){
-			return "some other function"
+		'selectedClass': function(){
+			var playerId = this._id;
+			var selectedPlayer = Session.get('selectedPlayer');
+			if(playerId == selectedPlayer){
+			  return "selected"
+			}
 		}
 	});
 }
